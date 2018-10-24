ecd10ee8922e26410afd352f0daa120c704486cb
jxc <3295574882@qq.com>
2018-10-24 13:50:23 +0800

    2018-10-24 13:50

--- MovieAnalysis/豆瓣top250/代码/可视化/top250_analysis.py
+++ MovieAnalysis/豆瓣top250/代码/可视化/top250_analysis.py
@@ -12,12 +12,12 @@ from collections import Counter
 os.chdir(r'C:\Users\Administrator\Desktop')
 
 
-# 豆瓣top250中各国所占比例分析
+# 豆瓣top250中各国电影所占比例分析
 def top250_area():
     ws = getSheet('top250.xlsx')
     data = ''
     j = 0
-    area = ws.col_values(2, start_rowx = 1)
+    area = ws.col_values(2, start_rowx=1)
     for i in area:
         data += i.replace(' ', '')
     jieba.add_word('中国大陆')  # 设置后，不对'中国大陆'进行再划分
@@ -36,7 +36,8 @@ def top250_area():
             break
     name = ''  # 图表名称
     pie = Pie('豆瓣top250中地区前二十所占比例分析')
-    pie.add(name, cls, num, is_label_show = True, legend_orient = 'vertical', legend_pos = 'right', radius = [28, 65])
+    pie.add(name, cls, num, is_label_show=True,
+            legend_orient='vertical', legend_pos='right', radius=[28, 65])
     pie.show_config()
     pie.render()
 
