38e6d51e83fcff38d617fbcdbbbd50c5bfec37ab
jxc <3295574882@qq.com>
2018-10-24 16:43:09 +0800

    项目搁置，准备毕业设计

--- Product/admin.py
+++ Product/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from Product.models import shop, product, category
+admin.site.register(shop)
+admin.site.register(product)
+admin.site.register(category)
--- User/admin.py
+++ User/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from User.models import user
+admin.site.register(user)
